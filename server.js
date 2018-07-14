4fe728a147f3871c2dde6862349589be8f62a375
ryan <lrcrews@gmail.com>
2018-07-14 15:17:10 -0700

    update to path in server/js for non-root redirects

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.listen(process.env.PORT || 8080);
 
 // Redirect direct URLs back to index so Angular may then route them properly
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/sim-city-calcit/index.html'));
 });
